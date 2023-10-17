969c491166c85b9e197ebdffd2d095f450b029fe
Lothar Schulz <lothar.sch@gmx.de>
2023-10-17 13:47:20 +0200

    chore gigasecond - tests pass

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -1,6 +1,6 @@
+use time::Duration;
 use time::PrimitiveDateTime as DateTime;
 
-// Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    todo!("What time is a gigasecond later than {start}");
+    start + Duration::seconds(1000000000) // note: a trailing semicolon would cause the test to fail 
 }
