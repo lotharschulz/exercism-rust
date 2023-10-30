e760525576b47909bcd289aa8e2942b6604e0d26
Lothar Schulz <lothar.sch@gmx.de>
2023-10-30 09:46:52 +0100

    chore armstrong numbers test cases pass

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,3 +1,11 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    todo!("true if {num} is an armstrong number")
+    let num_str = num.to_string();
+    let exponent = num_str.len() as u32;
+
+    let sum: u64 = num_str.chars()
+        .map(|x| x.to_digit(10).unwrap() as u64)
+        .map(|x| x.pow(exponent))
+        .sum();
+
+    sum == num as u64
 }
