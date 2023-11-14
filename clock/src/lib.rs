46a62e4c3711fadb6e4ac48e15294cf9d1ab0396
lotharschulz <lothar.schulz@buildingminds.com>
2023-11-14 11:58:19 +0100

    chore implementation started

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,11 +1,17 @@
-pub struct Clock;
+pub struct Clock {
+	hours: i32,
+	minutes: i32,
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        todo!("Construct a new Clock from {hours} hours and {minutes} minutes");
+        Clock{
+            hours: hours,
+            minutes: minutes
+        }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        todo!("Add {minutes} minutes to existing Clock time");
+        Clock::new(self.hours, self.minutes + minutes)
     }
 }
