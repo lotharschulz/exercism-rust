947a5fc8cbce9a75cb90e441951daa2f304561c6
lotharschulz <lothar.schulz@buildingminds.com>
2024-10-30 22:33:26 +0000

    chore solution passes all tests and is inspired by my kotlin solution https://github.com/lotharschulz/exercism-kotlin/commit/67104f4557b48be86e099904449adcbae9362454

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,3 +1,13 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    todo!("Sum the multiples of all of {factors:?} which are less than {limit}")
+    let mut multiples: Vec<u32> = Vec::new();
+    for n in 1..limit {
+        for &factor in factors {
+            if factor != 0 && n % factor == 0 {
+                multiples.push(n);
+            }
+        }
+    }
+    multiples.sort_unstable();
+    multiples.dedup();
+    multiples.iter().sum()
 }
