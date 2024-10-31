55f18c72b5e3235a10a79440d55079106a514ce3
lotharschulz <lothar.schulz@buildingminds.com>
2024-10-31 22:24:07 +0000

    chore sum of multiples with ranges, filters and iterators

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,13 +1,9 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    let mut multiples: Vec<u32> = Vec::new();
-    for n in 1..limit {
-        for &factor in factors {
-            if factor != 0 && n % factor == 0 {
-                multiples.push(n);
-            }
-        }
-    }
-    multiples.sort_unstable();
-    multiples.dedup();
-    multiples.iter().sum()
+    (1..limit)
+        .filter(|number| {
+            factors
+                .iter()
+                .any(|factor| *factor != 0 && number % factor == 0)
+        })
+        .sum()
 }
