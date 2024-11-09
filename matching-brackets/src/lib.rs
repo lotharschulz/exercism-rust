ac9ea56b5f1dee6b2903773666b7af2b398a4c03
Lothar Schulz <lothar.sch@gmx.de>
2024-11-10 00:21:49 +0100

    chore matching brackets solved

--- matching-brackets/src/lib.rs
+++ matching-brackets/src/lib.rs
@@ -1,3 +1,32 @@
+// 1) Declare a character stack brack as vector.
+// 2) Traverse the string expression.
+//     a) push char to stack, if the current character is a starting bracket (‘(‘ or ‘{‘ or ‘[‘).
+//     b) pop char from stack, if the current character is a closing bracket (‘)’ or ‘}’ or ‘]’),
+//            and if the popped character does not matches the starting bracket: then “not balanced”.
+// 3) After complete traversal, if there is some starting bracket left in stack: then “not balanced”.
+
 pub fn brackets_are_balanced(string: &str) -> bool {
-    todo!("Check if the string \"{string}\" contains balanced brackets");
+    let mut brack: Vec<char> = Vec::new();
+    for c in string.chars() {
+        match c {
+            '(' | '{' | '[' => brack.push(c),
+            ')' => {
+                if brack.pop() != Some('(') {
+                    return false;
+                }
+            }
+            '}' => {
+                if brack.pop() != Some('{') {
+                    return false;
+                }
+            }
+            ']' => {
+                if brack.pop() != Some('[') {
+                    return false;
+                }
+            }
+            _ => (),
+        }
+    }
+    brack.is_empty()
 }
