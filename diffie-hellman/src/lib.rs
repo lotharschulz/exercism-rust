20e994cd8417720b3d875267aee005f710288116
lotharschulz <lothar.schulz@buildingminds.com>
2024-11-14 08:26:36 +0100

    chore first implementation approach

--- diffie-hellman/src/lib.rs
+++ diffie-hellman/src/lib.rs
@@ -1,11 +1,17 @@
 pub fn private_key(p: u64) -> u64 {
-    todo!("Pick a private key greater than 1 and less than {p}")
+    number_between(2, p - 1)
+}
+
+fn number_between(min: u64, max: u64) -> u64 {
+    use rand::Rng;
+    let mut rng = rand::thread_rng();
+    rng.gen_range(min..=max)
 }
 
 pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
-    todo!("Calculate public key using prime numbers {p} and {g}, and private key {a}")
+    g.pow(a as u32) % p
 }
 
 pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
-    todo!("Calculate secret key using prime number {p}, public key {b_pub}, and private key {a}")
+    b_pub.pow(a as u32) % p
 }
