814aeb8503cb620c772187a098207cb9b617867c
lotharschulz <lothar.schulz@buildingminds.com>
2024-11-29 00:07:36 +0100

    chore all series tests pass

--- series/src/lib.rs
+++ series/src/lib.rs
@@ -1,3 +1,12 @@
 pub fn series(digits: &str, len: usize) -> Vec<String> {
-    todo!("What are the series of length {len} in string {digits:?}")
+    match len {
+        0 => vec!["".to_string()], // return a vector with an empty string if length is 0
+        l if l > digits.len() => vec![], // eeturn an empty vector if length is greater than the number of digits
+        _ => digits
+            .chars() // convert the string to a vector of characters
+            .collect::<Vec<_>>()
+            .windows(len) // create sliding windows of the specified length
+            .map(|window| window.iter().collect()) // convert each window to a string
+            .collect(), // collect all strings into a vector
+    }
 }
