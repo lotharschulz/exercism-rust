1eebbbf7d3df1d2b7026c17745c156e6077bd676
lotharschulz <lothar.schulz@buildingminds.com>
2024-12-19 21:18:37 +0100

    chore allergies tests passed

--- allergies/src/lib.rs
+++ allergies/src/lib.rs
@@ -1,27 +1,52 @@
-pub struct Allergies;
+pub struct Allergies {
+    score: u32,
+}
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum Allergen {
-    Eggs,
-    Peanuts,
-    Shellfish,
-    Strawberries,
-    Tomatoes,
-    Chocolate,
-    Pollen,
-    Cats,
+    Eggs = 1,
+    Peanuts = 2,
+    Shellfish = 4,
+    Strawberries = 8,
+    Tomatoes = 16,
+    Chocolate = 32,
+    Pollen = 64,
+    Cats = 128,
 }
 
 impl Allergies {
+    /// Constructs a new `Allergies` struct with the given score.
     pub fn new(score: u32) -> Self {
-        todo!("Given the '{score}' score, construct a new Allergies struct.");
+        Allergies { score }
     }
 
+    /// Determines if the patient is allergic to the given allergen.
     pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
-        todo!("Determine if the patient is allergic to the '{allergen:?}' allergen.");
+        // Check if the allergen bit is set in the score
+        self.score & (*allergen as u32) != 0
     }
 
+    /// Returns a list of allergens the patient is allergic to.
     pub fn allergies(&self) -> Vec<Allergen> {
-        todo!("Return the list of allergens contained within the score with which the Allergies struct was made.");
+        let mut result = Vec::new();
+        // Iterate over all possible allergens
+        for &allergen in [
+            Allergen::Eggs,
+            Allergen::Peanuts,
+            Allergen::Shellfish,
+            Allergen::Strawberries,
+            Allergen::Tomatoes,
+            Allergen::Chocolate,
+            Allergen::Pollen,
+            Allergen::Cats,
+        ]
+        .iter()
+        {
+            // Check if the patient is allergic to the current allergen
+            if self.is_allergic_to(&allergen) {
+                result.push(allergen);
+            }
+        }
+        result
     }
 }
