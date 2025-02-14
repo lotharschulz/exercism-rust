97853322e7fed87bae20e162e3ba307a1f842332
lotharschulz <lothar.schulz@buildingminds.com>
2025-02-14 19:23:20 +0100

    chore all queen-attack tests pass

--- queen-attack/src/lib.rs
+++ queen-attack/src/lib.rs
@@ -1,23 +1,34 @@
-#[derive(Debug)]
-pub struct ChessPosition;
+#[derive(Debug, PartialEq)]
+pub struct ChessPosition {
+    rank: i32,
+    file: i32,
+}
 
 #[derive(Debug)]
-pub struct Queen;
+pub struct Queen {
+    position: ChessPosition,
+}
 
 impl ChessPosition {
     pub fn new(rank: i32, file: i32) -> Option<Self> {
-        todo!(
-            "Construct a ChessPosition struct, given the following rank, file: ({rank}, {file}). If the position is invalid return None."
-        );
+        if rank >= 0 && rank < 8 && file >= 0 && file < 8 {
+            Some(ChessPosition { rank, file })
+        } else {
+            None
+        }
     }
 }
 
 impl Queen {
     pub fn new(position: ChessPosition) -> Self {
-        todo!("Given the chess position {position:?}, construct a Queen struct.");
+        Queen { position }
     }
 
     pub fn can_attack(&self, other: &Queen) -> bool {
-        todo!("Determine if this Queen can attack the other Queen {other:?}");
+        let same_row = self.position.rank == other.position.rank;
+        let same_column = self.position.file == other.position.file;
+        let same_diagonal = (self.position.rank - other.position.rank).abs()
+            == (self.position.file - other.position.file).abs();
+        same_row || same_column || same_diagonal
     }
 }
--- queen-attack/tests/queen-attack.rs
+++ queen-attack/tests/queen-attack.rs
@@ -7,35 +7,30 @@ fn queen_with_a_valid_position() {
 }
 
 #[test]
-#[ignore]
 fn queen_must_have_positive_row() {
     let chess_position = ChessPosition::new(-2, 2);
     assert!(chess_position.is_none());
 }
 
 #[test]
-#[ignore]
 fn queen_must_have_row_on_board() {
     let chess_position = ChessPosition::new(8, 4);
     assert!(chess_position.is_none());
 }
 
 #[test]
-#[ignore]
 fn queen_must_have_positive_column() {
     let chess_position = ChessPosition::new(2, -2);
     assert!(chess_position.is_none());
 }
 
 #[test]
-#[ignore]
 fn queen_must_have_column_on_board() {
     let chess_position = ChessPosition::new(4, 8);
     assert!(chess_position.is_none());
 }
 
 #[test]
-#[ignore]
 fn cannot_attack() {
     let white_queen = Queen::new(ChessPosition::new(2, 4).unwrap());
     let black_queen = Queen::new(ChessPosition::new(6, 6).unwrap());
@@ -43,7 +38,6 @@ fn cannot_attack() {
 }
 
 #[test]
-#[ignore]
 fn can_attack_on_same_row() {
     let white_queen = Queen::new(ChessPosition::new(2, 4).unwrap());
     let black_queen = Queen::new(ChessPosition::new(2, 6).unwrap());
@@ -51,7 +45,6 @@ fn can_attack_on_same_row() {
 }
 
 #[test]
-#[ignore]
 fn can_attack_on_same_column() {
     let white_queen = Queen::new(ChessPosition::new(4, 5).unwrap());
     let black_queen = Queen::new(ChessPosition::new(2, 5).unwrap());
@@ -59,7 +52,6 @@ fn can_attack_on_same_column() {
 }
 
 #[test]
-#[ignore]
 fn can_attack_on_first_diagonal() {
     let white_queen = Queen::new(ChessPosition::new(2, 2).unwrap());
     let black_queen = Queen::new(ChessPosition::new(0, 4).unwrap());
@@ -67,7 +59,6 @@ fn can_attack_on_first_diagonal() {
 }
 
 #[test]
-#[ignore]
 fn can_attack_on_second_diagonal() {
     let white_queen = Queen::new(ChessPosition::new(2, 2).unwrap());
     let black_queen = Queen::new(ChessPosition::new(3, 1).unwrap());
@@ -75,7 +66,6 @@ fn can_attack_on_second_diagonal() {
 }
 
 #[test]
-#[ignore]
 fn can_attack_on_third_diagonal() {
     let white_queen = Queen::new(ChessPosition::new(2, 2).unwrap());
     let black_queen = Queen::new(ChessPosition::new(1, 1).unwrap());
@@ -83,7 +73,6 @@ fn can_attack_on_third_diagonal() {
 }
 
 #[test]
-#[ignore]
 fn can_attack_on_fourth_diagonal() {
     let white_queen = Queen::new(ChessPosition::new(1, 7).unwrap());
     let black_queen = Queen::new(ChessPosition::new(0, 6).unwrap());
@@ -91,7 +80,6 @@ fn can_attack_on_fourth_diagonal() {
 }
 
 #[test]
-#[ignore]
 fn cannot_attack_if_falling_diagonals_are_only_the_same_when_reflected_across_the_longest_falling_diagonal(
 ) {
     let white_queen = Queen::new(ChessPosition::new(4, 1).unwrap());
