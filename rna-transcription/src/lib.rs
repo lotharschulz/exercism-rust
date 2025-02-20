df4e27c10cff8a3e650e3c853c51bf72b58b65b0
lotharschulz <lothar.schulz@buildingminds.com>
2025-02-20 21:54:34 +0100

    chore rna transcritpion implemented

--- rna-transcription/src/lib.rs
+++ rna-transcription/src/lib.rs
@@ -1,21 +1,52 @@
 #[derive(Debug, PartialEq, Eq)]
-pub struct Dna;
+pub struct Dna {
+    sequence: String,
+}
 
 #[derive(Debug, PartialEq, Eq)]
-pub struct Rna;
+pub struct Rna {
+    sequence: String,
+}
 
 impl Dna {
     pub fn new(dna: &str) -> Result<Dna, usize> {
-        todo!("Construct new Dna from '{dna}' string. If string contains invalid nucleotides return index of first invalid nucleotide");
+        for (i, nucleotide) in dna.chars().enumerate() {
+            match nucleotide {
+                'A' | 'C' | 'G' | 'T' => continue,
+                _ => return Err(i),
+            }
+        }
+        Ok(Dna {
+            sequence: dna.to_string(),
+        })
     }
 
     pub fn into_rna(self) -> Rna {
-        todo!("Transform Dna {self:?} into corresponding Rna");
+        let rna_sequence: String = self.sequence.chars().map(|nucleotide| {
+            match nucleotide {
+                'G' => 'C',
+                'C' => 'G',
+                'T' => 'A',
+                'A' => 'U',
+                _ => unreachable!(),
+            }
+        }).collect();
+        Rna {
+            sequence: rna_sequence,
+        }
     }
 }
 
 impl Rna {
     pub fn new(rna: &str) -> Result<Rna, usize> {
-        todo!("Construct new Rna from '{rna}' string. If string contains invalid nucleotides return index of first invalid nucleotide");
+        for (i, nucleotide) in rna.chars().enumerate() {
+            match nucleotide {
+                'A' | 'C' | 'G' | 'U' => continue,
+                _ => return Err(i),
+            }
+        }
+        Ok(Rna {
+            sequence: rna.to_string(),
+        })
     }
 }
