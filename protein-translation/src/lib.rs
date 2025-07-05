8e46e88e9e4a0a7c422cb1aa096610c690cf14cb
Lothar Schulz <lothar.sch@gmx.de>
2025-07-05 23:47:51 +0200

    chore protein translations solved

--- protein-translation/src/lib.rs
+++ protein-translation/src/lib.rs
@@ -1,5 +1,29 @@
 pub fn translate(rna: &str) -> Option<Vec<&str>> {
-    todo!(
-        "Return a list of protein names that correspond to the '{rna}' RNA string or None if the RNA string is invalid"
-    );
+    let mut proteins = Vec::new();
+    let codons = rna.as_bytes().chunks(3);
+
+    for codon_bytes in codons {
+        let codon = match std::str::from_utf8(codon_bytes) {
+            Ok(c) => c,
+            Err(_) => return None,
+        };
+
+        if codon.len() != 3 {
+            return None;
+        }
+
+        match codon {
+            "AUG" => proteins.push("Methionine"),
+            "UUU" | "UUC" => proteins.push("Phenylalanine"),
+            "UUA" | "UUG" => proteins.push("Leucine"),
+            "UCU" | "UCC" | "UCA" | "UCG" => proteins.push("Serine"),
+            "UAU" | "UAC" => proteins.push("Tyrosine"),
+            "UGU" | "UGC" => proteins.push("Cysteine"),
+            "UGG" => proteins.push("Tryptophan"),
+            "UAA" | "UAG" | "UGA" => return Some(proteins),
+            _ => return None,
+        }
+    }
+
+    Some(proteins)
 }
