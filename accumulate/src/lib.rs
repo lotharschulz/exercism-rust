fb953a834092618a52e995310f098e23332290ec
lotharschulz <lothar.schulz@buildingminds.com>
2025-09-14 22:51:48 +0200

    chore accumulate solved

--- accumulate/src/lib.rs
+++ accumulate/src/lib.rs
@@ -1,4 +1,15 @@
-/// What should the type of function be?
-pub fn map(input: Vec<i32>, function: ???) -> Vec<i32> {
-    todo!("Transform input vector {input:?} using passed function");
+/// Transform each element of the input collection using the provided function
+pub fn map<T, U, F, I>(input: I, mut function: F) -> Vec<U>
+where
+    F: FnMut(T) -> U,
+    I: IntoIterator<Item = T>,
+{
+    let input = input.into_iter();
+    let mut result = Vec::with_capacity(input.size_hint().0);
+
+    for item in input {
+        result.push(function(item));
+    }
+
+    result
 }
