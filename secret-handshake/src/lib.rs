064cf3f98e3f387782e3e29eab9f173ddb611fd6
Lothar Schulz <lothar.sch@gmx.de>
2026-01-23 15:14:46 +0100

    chore if else implementation

--- secret-handshake/src/lib.rs
+++ secret-handshake/src/lib.rs
@@ -5,21 +5,26 @@ pub fn actions(n: u8) -> Vec<&'static str> {
     // 4 (100): "close your eyes"
     // 8 (1000): "jump"
     // 16 (10000): reverse the order of the actions
-    // Convert the number to binary and determine which actions to include
-    let binary = format!("{:b}", n);
+
     // Create a vector to hold the actions
     let mut actions = Vec::new();
+
     // Check each bit and add the corresponding action
-    for (bit_pos, action) in [(0, "wink"), (1, "double blink"), (2, "close your eyes"), (3, "jump")] {
-        match (n >> bit_pos) & 1 {
-            1 => actions.push(action),
-            _ => {}
-        }
+    if n & 1 != 0 {
+        actions.push("wink");
+    }
+    if n & 2 != 0 {
+        actions.push("double blink");
+    }
+    if n & 4 != 0 {
+        actions.push("close your eyes");
+    }
+    if n & 8 != 0 {
+        actions.push("jump");
     }
     // Check if the reverse bit is set
-    match (n >> 4) & 1 {
-        1 => actions.reverse(),
-        _ => {}
+    if n & 16 != 0 {
+        actions.reverse();
     }
     actions   
 }
