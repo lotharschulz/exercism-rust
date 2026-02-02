542410607d2b015fa9f9636e10fde09688aa9562
Lothar Schulz <lothar.sch@gmx.de>
2026-02-02 20:58:13 +0100

    chore Reduce the Range of b: The inner loop can be optimized by limiting the range of b to a..=(sum - a) / 2. This avoids unnecessary iterations since b should not exceed c. Avoid Sorting: Since the triplet is always inserted in a sorted manner, you can directly insert the triplet as [a, b, c] without sorting. Early Exit: If a exceeds sum / 3, you can break out of the loop early since no valid triplet can be formed.

--- pythagorean-triplet/src/lib.rs
+++ pythagorean-triplet/src/lib.rs
@@ -3,13 +3,11 @@ use std::collections::HashSet;
 pub fn find(sum: u32) -> HashSet<[u32; 3]> {
     let mut triplets = HashSet::new();
 
-    for a in 1..sum {
-        for b in a..(sum - a) {
+    for a in 1..=sum / 3 {
+        for b in a..=(sum - a) / 2 {
             let c = sum - a - b;
-            if c > 0 && a * a + b * b == c * c {
-                let mut triplet = [a, b, c];
-                triplet.sort_unstable();
-                triplets.insert(triplet);
+            if a * a + b * b == c * c {
+                triplets.insert([a, b, c]);
             }
         }
     }
